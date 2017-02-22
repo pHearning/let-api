a1f69273f84c5e9f0f451a24dd97beef55181848
tedjohansson <tedjohanssondeveloper@gmail.com>
2017-02-22 08:24:20 +0000

    Comments and changing the name of the title call to make more sense.

--- app.py
+++ app.py
@@ -7,16 +7,32 @@ DATABASE = 'movies.db'
 app = Flask(__name__)
 
 
-# Create a factory to return dictionary instead of a list
 def dict_factory(cursor, row):
+    """
+    :param cursor:
+    :type cursor:
+    :param row:
+    :type row:
+    :return: a row formatted as a dictionary based on the cursor and row from the params
+    :rtype: dict
+
+    This function takes the standard format of a sqlite3 output and converts it
+    into a dictionary row by row.
+    """
     d = {}
     for idx, col in enumerate(cursor.description):
         d[col[0]] = row[idx]
     return d
 
 
-# Function to open the database and assign the row type
+
 def get_db():
+    """
+    :return: returns an open connection to a sqlite3 database
+    :rtype: sqlite3.Connection
+
+    A function that opens a connection to the sqlite database and assigns our custom row_factory.
+    """
     db = getattr(g, '_database', None)
     if db is None:
         connection = sqlite3.connect(DATABASE)
@@ -24,9 +40,16 @@ def get_db():
         db = g._database = connection
     return db
 
-# Close connection when we are done with it
+
 @app.teardown_appcontext
 def close_connection(exception):
+    """
+    :param exception:
+    :type exception:
+
+    When the app is closing it makes sure to close the database
+    as well using the teardown_appcontext decorator
+    """
     db = getattr(g, '_database', None)
     # If there is a database open we want to close it
     if db is not None:
@@ -35,6 +58,13 @@ def close_connection(exception):
 
 @app.route('/movies/api/v1.0/get_movies', methods=['GET'])
 def get_movies():
+    """
+    :return: get data of all movies in the database
+    :rtype: json
+
+    When a client calls the url in the route decorator
+    we return all the data in the movies table in a json format.
+    """
     # Open database and create a cursor
     cur = get_db().cursor()
     # Execute query
@@ -44,13 +74,25 @@ def get_movies():
     # Create json out of the dictionary and return it to the api
     return jsonify({'movies': res})
 
+
 @app.route('/movies/api/v1.0/get_movie', methods=['POST'])
 def get_movie():
+    """
+    :return: Requested movie
+    :rtype: json
+
+    When a client calls the url in the route decorator with a json containing the key value title
+    we return the information associated with that title in the movies table.
+
+    Improvements: We could create a better search function to retrieve the data.
+                  as of right now we need an absolute match to return data.
+    """
     # Open database and create a cursor
     cur = get_db().cursor()
-    # Execute query
+    # Execute query with the title based on request data from the post request
+    # Making sure we use the sqlite3 sql injection protection to protect from malicious attacks
     cur.execute('select * from movies where movie_title = ? order by movie_title',
-                (json.loads(request.data)['show_me_what_you_got'],))
+                (json.loads(request.data)['title'],))
     # Get the results from the executed query in a dictionary
     res = cur.fetchone()
     # Create json out of the dictionary and return it to the api
