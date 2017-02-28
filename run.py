f5ca7fe3e8c63f1cc643f3e115f16f1ebec75eb3
tedjohansson <tedjohanssondeveloper@gmail.com>
2017-02-28 18:19:54 +0000

    Added to requierments and the api_client

--- run.py
+++ run.py
@@ -4,4 +4,4 @@ if __name__ == "__main__":
     """
     A simpler way to start the flask app.
     """
-    app.run(debug=True)
+    app.run(debug=True, port=8000)
--- template/api_client.py
+++ template/api_client.py
@@ -1,6 +1,31 @@
 import requests
 
-url = 'http://127.0.0.1:5000/movies/api/v1.0/get_movie'
 
-r = requests.post(url, json={"title": "Avatar"})
-print(r.text)
+class Api:
+
+    def __init__(self):
+        self.url = 'http://127.0.0.1:8000/movies/api/v1.0/{0}'
+
+    def all_movies(self):
+        """
+        Retrieves all the movies in the API.
+        :return:
+        :rtype:
+        """
+        return requests.get(self.url.format('get_movies'))
+
+    def find_movie(self, title):
+        """
+        Retrieves a movie from the API based on the title field.
+        :param title:
+        :type title:
+        :return:
+        :rtype:
+        """
+
+        return requests.post(self.url.format('get_movie'), json={'title': title})
+
+if __name__ == '__main__':
+    api = Api()
+
+    print(api.find_movie('Avatar').text)
